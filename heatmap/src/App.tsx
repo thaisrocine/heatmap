d180b91e4c7b8235276223bad65bcc5b2215cd01
thaisrocine_ <69332861+thaisrocine@users.noreply.github.com>
2024-08-21 07:30:57 -0300

    commit

--- heatmap/src/App.tsx
+++ heatmap/src/App.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
-import HeatmapWithScreenshot from './heatmap-data/HeatmapWithScreenshot';
-
+import React, { useState } from 'react';
 
 const App: React.FC = () => {
+  const [url, setUrl] = useState('https://www.cea.com.br/');
+  const predefinedUrls = [
+    'https://www.cea.com.br/',
+    'https://www.example.com/',
+    'https://www.anotherexample.com/',
+  ];
+
   return (
     <div>
       <h1>Heatmap Example</h1>
-      <HeatmapWithScreenshot
-        url="https://www.google.com/"
-        csvFilePath="/csv-data/data.csv"
+      <select
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+        style={{ width: '100%', padding: '10px', marginBottom: '10px' }}
+      >
+        {predefinedUrls.map((predefinedUrl) => (
+          <option key={predefinedUrl} value={predefinedUrl}>
+            {predefinedUrl}
+          </option>
+        ))}
+      </select>
+      <iframe
+        src={url}
+        style={{ width: '100%', height: '100vh', border: 'none' }}
+        title="Dynamic Content"
       />
     </div>
   );
